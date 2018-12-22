595268920cf6127e7fcdcc13da19a5ae63a36f8e
fritz <fritzvillacorta@gmail.com>
2018-12-22 12:40:00 +0000

    Inserted chartjs for visualization

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,11 @@
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
   
+  var tense =0;
+  var neutral =0;
+  var confident =0;
+  var desperate =0;
+
   let isVideoRun = true
   let isLoadedMetaData = false
   let constraints = { audio: false, video: {facingMode: 'user'} }
@@ -92,18 +97,69 @@
   }, false)
 
   rearBtn.addEventListener('click', () => {
-    stop()
-    constraints.video.facingMode = 'environment'
-    setTimeout( () => {
-      start()
-    }, 500)
+    
+    let myChart = document.getElementById('myChart').getContext('2d');
+
+    // Global Options
+    Chart.defaults.global.defaultFontFamily = 'Lato';
+    Chart.defaults.global.defaultFontSize = 18;
+    Chart.defaults.global.defaultFontColor = '#777';
+
+    let massPopChart = new Chart(myChart, {
+      type:'pie', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+      data:{
+        labels:['Confidence', 'Tense', 'Desperation', 'Neutral Emotion'],
+        datasets:[{
+          label:'Population',
+          data:[
+            617594,
+            181045,
+            153060,
+            106519,
+          ],
+          //backgroundColor:'green',
+          backgroundColor:[
+            'rgba(255, 99, 132, 0.6)',
+            'rgba(54, 162, 235, 0.6)',
+            'rgba(255, 206, 86, 0.6)',
+            'rgba(75, 192, 192, 0.6)',
+          ],
+          borderWidth:1,
+          borderColor:'#777',
+          hoverBorderWidth:3,
+          hoverBorderColor:'#000'
+        }]
+      },
+      options:{
+        title:{
+          display:true,
+          text:'Dominant Emotion for the Interview',
+          fontSize:25
+        },
+        legend:{
+          display:true,
+          position:'right',
+          labels:{
+            fontColor:'#000'
+          }
+        },
+        layout:{
+          padding:{
+            left:50,
+            right:0,
+            bottom:0,
+            top:0
+          }
+        },
+        tooltips:{
+          enabled:true
+        }
+      }
+    });
+
   }, false)
   
-  var tense =0;
-  var neutral =0;
-  var confident =0;
-  var desperate =0;
-
+  
   function detectFace(){
     ctx.drawImage(video, 0, 0)
 
